d3e006775f306c6881a95f7b4c3b311155f95722
Elias Delgado <evdelgado99@gmail.com>
2020-08-27 08:47:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,32 @@
 # Write your methods here
+
+# loop_message_five_times
+
+def loop_message_five_times(message)
+  
+  5.times {puts message}
+
+end
+
+# loop_message_n_times
+
+def loop_message_n_times(message, integer)
+
+integer = 0
+
+  while message do
+    puts message
+    integer += 1
+  end
+
+end
+
+# output_array
+
+def output_array
+  
+  
+
+end
+
+# return_string_array
